c99d72cb493ea38d2bbcc4c7bd6794ddf75694df
RomanVr <romakf99@yandex.ru>
2022-02-23 12:26:56 +0300

    add docker files mysql

---
+++ Mysql-Docker/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS orderManSys;
+USE orderManSys;
+
+-- 1 ************************************** Organization
+
+CREATE TABLE IF NOT EXISTS Organization
+(
+ id             int PRIMARY KEY AUTO_INCREMENT,
+ NameOrganization varchar(250) UNIQUE NOT NULL DEFAULT '',
+ Location         varchar(250) NOT NULL DEFAULT '',
+ TypeOrganization varchar(250) NOT NULL DEFAULT '',
+ LastUpdate       timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
